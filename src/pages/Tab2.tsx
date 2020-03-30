f974ba54416b4b587357fe965948c0ae67b57c8e
boris <basarkis@alumni.unca.edu>
2020-03-30 17:35:56 -0400

    import usePhotoGallery and add takePhoto const

--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -5,8 +5,10 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar,
          IonCol, IonImg, IonActionSheet } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Tab2.css';
+import { usePhotoGallery } from "../hooks/usePhotoGallery";
 
 const Tab2: React.FC = () => {
+  const { takePhoto } = usePhotoGallery();
   return (
     <IonPage>
       <IonHeader>
